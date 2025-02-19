6cf957f565609ac135a84ec1bd330b5f38da9d6d
Gajapathi K <gajapathi787@gmail.com>
2025-02-19 17:37:23 +0530

    today_update

--- src/Components/Student/Hero.jsx
+++ src/Components/Student/Hero.jsx
@@ -22,6 +22,7 @@ const Hero = () => {
         <SearchBar/>
         <Companies/>
         <CourseSection/>
+        
       </div>
 
 
--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,15 +1,47 @@
-import {createContext} from "react";
+// AppContext.jsx
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-export const AppContext=createContext()
+export const AppContext = createContext();
 
-export const AppContextProvider=(props)=>{
-    const value={
+export const AppContextProvider = (props) => {
+    const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate();
+    const [allCourses, setAllCourses] = useState([]);
 
-    }
+    const fetchallCourses = async () => {
+        setAllCourses(dummyCourses);
+    };
 
-    return(
+    useEffect(() => {
+        fetchallCourses();
+    }, []);
+    const calculateRating = (course) => {
+        if (course.courseRatings.length === 0) {
+            return 0;
+        }
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating;
+        });
+        return totalRating / course.courseRatings.length;
+    };
+    
+    const value = {
+        currency,
+        allCourses,
+        navigate,
+        calculateRating
+    };
+
+
+    
+    
+
+    return (
         <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
-    )
-}
+    );
+};
