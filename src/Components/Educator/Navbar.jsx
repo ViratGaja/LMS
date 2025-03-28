ce629e03dac44d178073f94a4d89f3f9b1257573
Gajapathi K <gajapathi787@gmail.com>
2025-03-28 15:40:13 +0530

    sm

--- src/Components/Educator/Navbar.jsx
+++ src/Components/Educator/Navbar.jsx
@@ -9,11 +9,14 @@ const Navbar = () => {
   const educatorData=dummyEducatorData
   const {user}=useUser()
   return (
-    <div>
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3'>
       <Link to='/'>
       <img src={assets.logo} alt="" />
       </Link>
-        <h1>JEllo</h1>
+      <div className='flex items-center gap-5 text-green-500 relative'>
+        <p>Hi! {user?user.fullName:"Developers"}</p>
+        {user ? <UserButton/>: <img src={assets.profile_img} className='max-w-8' alt="" /> }
+      </div>
     
     </div>
   )
