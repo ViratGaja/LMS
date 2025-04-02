f3dbe7cf86d0f773a05ebd57a0fdc8ad6b142335
Gajapathi K <gajapathi787@gmail.com>
2025-04-02 21:45:31 +0530

    change

--- src/Pages/Educator/Educator.jsx
+++ src/Pages/Educator/Educator.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../Components/Educator/Navbar'
 const Educator = () => {
   return (
     <div>
-    
+    <div className='text-default min-h-screen bg-white'
+    >
+      <Navbar/>
+    </div>
       <div>
         <Outlet />
       </div>
